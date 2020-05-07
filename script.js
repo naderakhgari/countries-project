6a387de30cf256181a93eda9a76e3b926c82a4f8
Nader Akhgari <nader90akhgari@gmail.com>
2020-05-07 02:39:49 +0100

    added name and flag of countries to the page

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+const counttriContainer = document.querySelector(".countries");
 fetch("https://restcountries.eu/rest/v2/all")
 .then(Response => {
     return Response.json();
 })
-.then(data => {
-    console.log(data);
-    // console.log(data.region)
-    data.forEach(d => console.log(d.region))
+.then(countries => {
+    console.log(countries);
+   
+    countries.forEach(countrie => {
+        counttriContainer.innerHTML += `
+        <div class="countrie lg-col-6">
+            <h2 class="countrieName lg-col-10">${countrie.name}</h2>
+            <img src="${countrie.flag}" class=" flag lg-col-4">
+        </div>`
+    })
 })
